567330d6f7e095e78867862a30c3045a81098d1e
ksphub <sivaprasad.kammara@gmail.com>
2019-03-08 11:32:54 +0000

    Added files 2

---
+++ kspscripts/terraformtfvars.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+aws_access_key_id=$(cat ~/.aws/credentials | grep -i aws_access_key_id | awk -F " " '{ print $3 }')
+aws_secret_access_key=$(cat ~/.aws/credentials | grep -i aws_secret_access_key | awk -F " " '{ print $3 }')
+TFVAR="/var/lib/jenkins/test/terraform.tfvars"
+touch $TFVAR ; > $TFVAR
+echo 'aws_access_key_id = "kspaccessid"' > $TFVAR
+echo 'aws_secret_access_key = "kspsecretid"' >> $TFVAR
+sed -i 's/kspaccessid/'$aws_access_key_id'/g' $TFVAR
+sed -i 's/kspsecretid/'$aws_secret_access_key'/g' $TFVAR
