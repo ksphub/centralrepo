342b2cb69ce91792848bad2e72fe9c781844fc5b
ksphub <31971823+ksphub@users.noreply.github.com>
2019-03-10 17:34:34 +0530

    Updated Script

--- kspscripts/jenkinsinstall.sh
+++ kspscripts/jenkinsinstall.sh
@@ -5,4 +5,6 @@ sudo sed -ei '\\$aJAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64/jre/bin/java' 
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -  && 
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'  && 
 sudo apt-get update  && 
-sudo apt-get install jenkins -y
+sudo apt-get install jenkins -y &&
+sudo apt-get install awscli -y &&
+echo "jenkins ALL=(ALL) NOPASSWD:ALL" | sudo tee --append  /etc/sudoers.d/90-cloud-init-users
