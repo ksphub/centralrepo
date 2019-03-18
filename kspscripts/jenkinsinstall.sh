5ea72e427610fc6829759400d2e484260bd2f476
ksphub <31971823+ksphub@users.noreply.github.com>
2019-03-19 01:16:51 +0530

    Update jenkinsinstall.sh

--- kspscripts/jenkinsinstall.sh
+++ kspscripts/jenkinsinstall.sh
@@ -13,4 +13,4 @@ sudo apt-get install maven -y
 #ls -al /etc/alternatives/java
 #sudo update-alternatives --config java
 #sudo sed -i -e "\$aJAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64/jre/bin/java" /etc/environment
-#sudo sed -ie '$aJAVA_HOME=/usr/lafib/jvm/java-1.8.0-openjdk-amd64/jre/bin/java' /etc/environment
+#sudo sed -ie '$aJAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64/jre/bin/java' /etc/environment
