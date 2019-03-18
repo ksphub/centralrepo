fd54d211deb8373565ca5651f6415e2b2c2d3617
ksphub <31971823+ksphub@users.noreply.github.com>
2019-03-18 12:51:27 +0530

    Update jenkinsinstall.sh

--- kspscripts/jenkinsinstall.sh
+++ kspscripts/jenkinsinstall.sh
@@ -2,6 +2,7 @@
 sudo apt-get update && 
 sudo apt install openjdk-8-jdk wget -y  && 
 sudo sed -ei '\\$aJAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64/jre' /etc/environment
+source /etc/environment
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -  && 
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'  && 
 sudo apt-get update  && 
