50986c19d4873a7c247929597c55322440bdff22
kspub <sivaprasad.kammara@gmail.com>
2019-03-26 15:55:06 +0000

    modidfied terraform script

--- kspscripts/terraformtfvars.sh
+++ kspscripts/terraformtfvars.sh
@@ -2,6 +2,7 @@
 aws_access_key_id=$(cat ~/.aws/credentials | grep -i aws_access_key_id | awk -F " " '{ print $3 }')
 aws_secret_access_key=$(cat ~/.aws/credentials | grep -i aws_secret_access_key | awk -F " " '{ print $3 }')
 TFVAR="/var/lib/jenkins/tftasks/terraform.tfvars"
+mkdir /var/lib/jenkins/tftasks/
 touch $TFVAR ; > $TFVAR
 echo 'aws_access_key_id = "kspaccessid"' > $TFVAR
 echo 'aws_secret_access_key = "kspsecretid"' >> $TFVAR
