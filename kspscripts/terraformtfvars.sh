4df628d10d8867226cd6b7823fde173a94e118d8
ksphub <sivaprasad.kammara@gmail.com>
2019-03-26 16:11:30 +0000

    modified terraform

--- kspscripts/terraformtfvars.sh
+++ kspscripts/terraformtfvars.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+sudo apt-get update
+sudo apt-get install unzip wget -y
+wget https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_amd64.zip
+unzip terraform_0.11.11_linux_amd64.zip
 aws_access_key_id=$(cat ~/.aws/credentials | grep -i aws_access_key_id | awk -F " " '{ print $3 }')
 aws_secret_access_key=$(cat ~/.aws/credentials | grep -i aws_secret_access_key | awk -F " " '{ print $3 }')
 TFVAR="/var/lib/jenkins/tftasks/terraform.tfvars"
